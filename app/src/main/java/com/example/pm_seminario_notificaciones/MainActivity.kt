cdfb8b93f2ccbe1c94c04955efe7d3c46c55eeba
Jimmyh091 <jaimecaballero164@gmail.com>
2025-01-17 16:14:02 +0100

    Ejercicio 1: Crear canal y notifiación

--- app/src/main/java/com/example/pm_seminario_notificaciones/MainActivity.kt
+++ app/src/main/java/com/example/pm_seminario_notificaciones/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.pm_seminario_notificaciones
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +22,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        mostrar_notificacion(this)
+    }
+
+    fun mostrar_notificacion(context: Context){
+
+        // Crear el canal
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "canal1"
+            val channelName = "Canal de prueba"
+            val notificationChannel  = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+            context.getSystemService<NotificationManager>()?.createNotificationChannel(notificationChannel)
+        }
+
+        // Crear la notificacion
+        val builder = NotificationCompat.Builder(context, "canal1")
+            .setSmallIcon(R.drawable.pizza)
+            .setContentTitle("Titulo de prueba")
+            .setContentText("Texto de prueba")
+            .setColor(ContextCompat.getColor(context, R.color.salmon))
+
+        // Mostrar la notificacion
+        with(context.getSystemService<NotificationManager>()) {
+            this?.notify(1, builder.build())
         }
     }
 }
